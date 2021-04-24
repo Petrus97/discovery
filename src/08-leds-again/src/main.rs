8688fcf7256b263001f3c1194ceb6579c321f14a
ale19 <ale.petru97@gmail.com>
2021-04-24 17:38:37 +0200

    light up leds by initializing registers

--- src/08-leds-again/src/main.rs
+++ src/08-leds-again/src/main.rs
@@ -9,6 +9,19 @@ fn main() -> ! {
     let (gpioe, rcc) = aux8::init();
 
     // TODO initialize GPIOE
+    rcc.ahbenr.write(|w| w.iopeen().set_bit());
+    // rcc.apb1enr.write(|w| w.pwren().set_bit());
+    
+    gpioe.moder.write(|w| {
+        w.moder9().output(); // LD3
+        w.moder8().output(); // LD4
+        w.moder10().output(); // LD5
+        w.moder15().output(); // LD6
+        w.moder11().output(); // LD7
+        w.moder14().output(); // LD8
+        w.moder12().output(); // LD9
+        w.moder13().output() // LD10
+    });
 
     // Turn on all the LEDs in the compass
     gpioe.odr.write(|w| {
