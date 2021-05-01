d4ffaf9554a91f8e80ae13abefceacfff6778961
ale19 <ale.petru97@gmail.com>
2021-05-01 20:17:57 +0200

    adjust micro to usart baudrate

---
+++ src/11-usart/Notes.md
@@ -0,0 +1,22 @@
+# Using `continue` in gdb
+It miss order in the phrase
+```
+Theqic bon oxjup oerte az dg
+```
+# Using `step & next` in gdb
+The phrase is sended correctly
+```
+The quic
+```
+# Using ISR register (Status Register)
+The status register (ISR) has a flag, TXE, that indicates if it's "safe" to write to the TDR register without incurring in data loss.
+```
+The quick brown fox jumps over the lazy dog.
+```
+
+## `itmdump` log
+This is the time elapsed by microcontroller to process the string
+```
+`for` loop took 19688 ticks (2461 us)  // without ISR register 
+`for` loop took 30557 ticks (3819.625 us) // with the ISR register
+```
--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -7,10 +7,37 @@ use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let (usart1, mono_timer, itm) = aux11::init();
+    let (usart1, mono_timer, mut itm) = aux11::init();
 
     // Send a single character
-    usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
+    // usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
+    // new line
+    usart1.tdr.write(|w| w.tdr().bits(u16::from(b'\n')));
+
+
+    // Since serial can send 11.52 KB/s of data, our 45 byte string 
+    // is going to take (45 bytes / (11,520 bytes/s) = 3,906 us
+    // to send the string.
+    // The processor works at 8MHz, exec an instruction takes 125 ns
+    // The for loop process the string in less than 3.906 us!
+    let instant = mono_timer.now();
+    // Send a string
+    let s = "The quick brown fox jumps over the lazy dog.";
+    for c in s.as_bytes(){
+        // wait until it's safe to write
+        while usart1.isr.read().txe().bit_is_clear() {}
+        // Now is safe to write without "overrun the buffer"
+        usart1.tdr.write(|w| w.tdr().bits(u16::from(*c)));
+    }
+    let elapsed = instant.elapsed();
+
+    iprintln!(
+        &mut itm.stim[0],
+        "`for` loop took {} ticks ({} us)",
+        elapsed,
+        elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
+    );
+
 
     loop {}
 }
