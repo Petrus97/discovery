5e57ab52982a6a4873b9715372b0bbdf94bc20d2
ale19 <ale.petru97@gmail.com>
2021-05-08 16:09:42 +0200

    read more registers using i2c

--- src/14-i2c/src/main.rs
+++ src/14-i2c/src/main.rs
@@ -17,63 +17,68 @@ const MAGNETOMETER_ID: u8 = 0x4f; // WHO_AM_I_M
 
 #[entry]
 fn main() -> ! {
-    let (i2c1, _delay, mut itm) = aux14::init();
+    let (i2c1, mut delay, mut itm) = aux14::init();
+    
+    iprintln!(&mut itm.stim[0], "start:");
 
-    iprintln!(&mut itm.stim[0], "check itm");
-
-    // Stage 1: Send the address of the register we want to read to the
-    // magnetometer
-    {
-        // TODO Broadcast START
+    loop {
+        // Initialize i2c communication to magnetometer
+        // Broadcast START
+        // Broadcast the MAGNETOMETER address with the R/W bit set to Write
         i2c1.cr2.write(|w| {
             w.start().set_bit();
             w.sadd1().bits(MAGNETOMETER_ADDR);
-            w.rd_wrn().clear_bit();
-            w.nbytes().bits(1);
-            w.autoend().clear_bit()
+            w.rd_wrn().clear_bit(); // Write
+            w.nbytes().bits(1); // 1 byte of data to read (address)
+            w.autoend().clear_bit() // Do not close the communication after NBYTES
         });
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Write
-        // i2c1.cr2.write(|w| w.sadd1().bits(MAGNETOMETER_ADDR));
-        // i2c1.cr2.write(|w| w.rd_wrn().clear_bit());
+        // Wait until we can send more data
+        while i2c1.isr.read().txe().bit_is_clear() {}
 
-        // TODO Send the address of the register that we want to read: IRA_REG_M / WHO_AM_I_M
-        // i2c1.cr2.write(|w| w.nbytes().bits(1));
-        // i2c1.cr2.write(|w| w.autoend().clear_bit());
+        // Send the address we want to read: OUTX_H_REG_M
+        i2c1.txdr.write(|w| w.txdata().bits(OUTX_H_REG_M));
 
-        while i2c1.isr.read().txis().bit_is_clear() {} // This bit is set when I2C_TXDR register is empty. It is cleared when the next data to be sent is written in the I2C_TXDR register.
-
-        i2c1.txdr.write(|w| w.txdata().bits(MAGNETOMETER_ID));
-        
         while i2c1.isr.read().tc().bit_is_clear() {}
-    }
 
-    // Stage 2: Receive the contents of the register we asked for
-    let byte = {
-        // TODO Broadcast RESTART
+        // Now we want to read 6 contiguous registers of the magnetometer that expose sensor reading
+
+        // Broadcast RESTART
+        // Broadcast the MAGNETOMETER address with the R/W bit set to Read
         i2c1.cr2.write(|w| {
             w.start().set_bit();
-            w.sadd1().bits(MAGNETOMETER_ADDR); // This is not important since the bus is used only by the master and slave
-            w.rd_wrn().set_bit();
-            w.nbytes().bits(1)
+            w.sadd1().bits(MAGNETOMETER_ADDR);
+            w.rd_wrn().set_bit(); // Read
+            w.nbytes().bits(6); // 6 bytes to read
+            w.autoend().set_bit()
         });
-        // TODO Broadcast the MAGNETOMETER address with the R/W bit set to Read
-        // i2c1.cr2.write(|w| w.sadd1().bits(MAGNETOMETER_ADDR));
-        // i2c1.cr2.write(|w| w.rd_wrn().set_bit());
-        // i2c1.cr2.write(|w| w.nbytes().bits(1));
 
-        // TODO Receive the contents of the register
-        while i2c1.isr.read().rxne().bit_is_clear() {} // This bit is set by hardware when the received data is copied into the I2C_RXDR register.
+        let mut buffer = [0_u8; 6];
+        // iprintln!(&mut itm.stim[0], "{:?}", buffer);
 
-        // TODO Broadcast STOP
-        i2c1.cr2.write(|w| w.stop().set_bit());
+        for byte in &mut buffer {
+            // Wait until we have received the contents of the register
+            while i2c1.isr.read().rxne().bit_is_clear() {}
 
-        i2c1.rxdr.read().rxdata().bits()
-        // 0
-    };
+            *byte = i2c1.rxdr.read().rxdata().bits();
+        }
+        // Broadcast STOP (automatic because of `AUTOEND = 1`)
 
-    // Expected output: 0x0A - 0b01001000
-    // Expected output: 0x4f - 0b01000000
-    iprintln!(&mut itm.stim[0], "0x{:02X} - 0b{:08b}", MAGNETOMETER_ID, byte);
+        // iprintln!(&mut itm.stim[0], "{:?}", buffer);
 
-    loop {}
+        let x_h = u16::from(buffer[0]); // high byte
+        let x_l = u16::from(buffer[1]); // low byte
+        let z_h = u16::from(buffer[2]);
+        let z_l = u16::from(buffer[3]);
+        let y_h = u16::from(buffer[4]);
+        let y_l = u16::from(buffer[5]);
+
+        let x = ((x_h << 8) + x_l) as i16; // Can be negative
+        let y = ((y_h << 8) + y_l) as i16;
+        let z = ((z_h << 8) + z_l) as i16;
+
+        iprintln!(&mut itm.stim[0], "{:?}", (x, y, z));
+
+        delay.delay_ms(1_000_u16);
+    }
+
 }
