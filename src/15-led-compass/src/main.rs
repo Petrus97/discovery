cd4cea309d86758168071053bff2407d513660c2
ale19 <ale.petru97@gmail.com>
2021-05-08 19:31:06 +0200

    Switch on leds based on unscaled x,y data

--- src/15-led-compass/src/main.rs
+++ src/15-led-compass/src/main.rs
@@ -3,12 +3,12 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use aux15::{entry, iprint, iprintln, prelude::*};
+use aux15::{entry, iprint, iprintln, prelude::*, Direction};
 
 #[entry]
 fn main() -> ! {
     let init_vars = aux15::init();
-    let leds = init_vars.0;
+    let mut leds = init_vars.0;
     let lsm303agr = init_vars.1;
     let mut delay = init_vars.2;
     let mut itm = init_vars.3;
@@ -18,7 +18,25 @@ fn main() -> ! {
     let mut lsm303agr = lsm303agr.into_mag_continuous().ok().unwrap();
 
     loop {
-        iprintln!(&mut itm.stim[0], "{:?}", lsm303agr.mag_data().unwrap());
-        delay.delay_ms(2_000_u16);
+        let measure = lsm303agr.mag_data().unwrap();
+
+        iprintln!(&mut itm.stim[0], "{:?}", measure);
+        // Look at the signs of the X and Y components to determine in which
+        // quadrant the magnetic field is
+        let direction = match (measure.x > 0, measure.y > 0) {
+            // Quadrant I
+            (true, true) => Direction::Southeast,
+            // Quadrant II
+            (false, true) => Direction::Northeast,
+            // Quadrant III
+            (false, false) => Direction::Northwest,
+            // Quadrant IV
+            (true, false) => Direction::Southwest,
+        };
+        
+        leds.iter_mut().for_each(|led| led.off());
+        leds[direction].on();
+
+        delay.delay_ms(1_000_u16);
     }
 }
